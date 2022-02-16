b6d2b53d713399e8855f3eaecb7e9614a8c83070
notebook <yeogunna@gmail.com>
2022-02-16 15:42:26 +0900

    init

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,29 @@
+package hello.typeconverter.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request){
+        String data = request.getParameter("data");
+        Integer intValue = Integer.valueOf(data);
+        log.info("hello-v1 intValue={}", intValue);
+
+        return "ok";
+    }
+
+    @GetMapping("hello-v2")
+    public String helloV2(@RequestParam Integer data){
+        log.info("hello-v2 data={}",data);
+        return "ok";
+    }
+
+}
